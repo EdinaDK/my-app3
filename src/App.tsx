077c932ceb1ae5819cea37af76e4143892a70653
EdinaDK <edinadk@mer.ci.nsu.ru>
2022-10-06 11:47:09 +0700

    06.10

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import './App.css';
-import {Component} from "./components/Component";
+import {Simulate} from "react-dom/test-utils";
+import input = Simulate.input;
+import {Property} from "csstype";
+import Filter = Property.Filter;
+//import {Component} from "./components/Component";
 /*import {OnlyEven} from "./components/OnlyEven";
 import {Temperature} from "./components/Temperature";
 
@@ -16,8 +20,26 @@ function Component({ n }: ComponentProps) {
 
 function App() {
 
-  //return <><Component n ={3}/> <OnlyEven arr ={[1, 2, 3, 4, 5]}/> <Temperature t={-451}/> </>;
-  return <div className="App"><Component/></div>;
+    //return <><Component n ={3}/> <OnlyEven arr ={[1, 2, 3, 4, 5]}/> <Temperature t={-451}/> </>;
+    //return <div className="App"><Component/></div>;
+
+    return <div className="App">
+            <form onSubmit={submitHandler}>
+                <input value={input} onChange={e => setInput(e.target.value)}/>
+                <button type="submit">+</button>
+            </form>
+            {Object.values(Filter).map(filterValue =>
+                <label>
+                    <input
+                        checked={filter === filterValue}
+                        type="radio"
+                        name="filter"
+                        onChange={() => setFilter(filterValue)}/>
+                    {filterValue}
+                </label>
+            )}
+            
+        </div>;
 }
 
 export default App;
